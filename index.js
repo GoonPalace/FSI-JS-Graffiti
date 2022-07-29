60ef7b4978786808fdf2742fe62586fcdd46591f
GoonPalace <noahfbinsaid@gmail.com>
2022-07-28 19:53:24 -0700

    some silly changes

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Select some elements...
+
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i<dogImages.length; i++)
+{
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = "rotate(180deg)"
+}
+
+let dogName = document.querySelectorAll ('.dog-name')
+for(let i = 0; i<dogName.length; i++)
+{
+    dogName[i].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'orange'
+footer.style.borderStyle = 'solid'
+
+let dogDescription = document.querySelectorAll ('.dog-description')
+for(let i = 0; i<dogDescription.length; i++)
+{
+    dogDescription[i].style.textAlign = 'justify'
+}
